c803f518191db822410c6204fa520f22e94091ab
Yoghurt1 <eoinmccoy@me.com>
2016-02-05 23:16:19 +0000

    Update simple-calculator.cpp V3.0
    Validates user input so program doesn't break and loop.

--- simple-calculator.cpp
+++ simple-calculator.cpp
@@ -6,23 +6,36 @@ Credit to github.com/DanTheManRM for bork variable :^)
 Changelog
   V1.0 - Original program
   V2.0 - Added functionality for restarting the program if the user enters an invalid equation or if the user wants to enter another equation.
+  V3.0 - Added cinCheck() function to check if the input for number1 and number2 was int only.
 */
 
 #include <iostream>
 using namespace std;
 
-int number1, number2, answer, bork; //Initialize integers number1, number2, answer and bork
-std::string function; //Initialize string for operator and restarting the program
 std::string restart = "n"; //Set restart to "n"
+
+int cinCheck() //Function for checking cin for number1 and number2
+{
+  if ( !cin ) { //If cin is invalid
+    cin.clear(); //Clear failbit
+  }
+
+  return 0; //End cinCheck()
+}
 
 int main()
 {
+  int number1, number2, answer; //Initialize integers number1, number2, answer
+  bool bork = false; //Initialize bork for if the program breaks
+  std::string function; //Initialize string for operator and restarting the program
+
   if (restart != "y") { //Ensures this only displays first time the program runs
     cout << "Welcome to Simple Calculator V1.1!\n";
   }
 
   cout << "Enter an equation:\n"; //Asks user to input 2 numbers
   cin >> number1 >> function >> number2; //Sets number1, operator and number2 to equation, use spaces my dood ;^)
+  cinCheck();
 
   if ( function == "+" ) { //If-else statement for each operator
     answer = number1 + number2; //If add
@@ -30,22 +43,24 @@ int main()
   else if ( function == "-" ) {
     answer = number1 - number2; //If subract
   }
-  else if ( function == "x" ) {
+  else if ( function == "*" ) {
     answer = number1 * number2; //If multiply
   }
   else if ( function == "/" ) {
     answer = number1 / number2; //If divide
   }
   else {
-    bork = true;
+    bork = true; //If invalid function input, set bork to true
   }
-  if ( bork = true ) {
-    cout << "Please enter a valid equation (+, -, x, /).\n";
-    restart = "y";
-    main();
+
+  if ( bork == true ) { //If bork is true
+    cout << "Please enter a valid equation (+, -, x, /) or ensure you have used spaces in your equation (e.g. 1 + 1).\n"; //Ask user to put in a valid input
+    restart = "y"; //Set the restart variable to "y" so the welcome message doesn't display again.
+    main(); //Restart main() function
   }
-  cout << "Answer to entered equation = " << answer << endl; //Prints the answer
+  else { //If bork is false
+    cout << "Answer to entered equation = " << answer << endl; //Prints the answer
+  }
 
-  return 0;
-
+  return 0; //End main() function
 }
