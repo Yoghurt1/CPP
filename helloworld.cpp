bd306c8046617cc485f378c1d1db69a0efc62df9
Yoghurt1 <eoinmccoy@me.com>
2016-02-05 20:39:59 +0000

    Create helloworld.cpp
    First C++ program, prints hello world.

---
+++ helloworld.cpp
@@ -0,0 +1,12 @@
+/*
+First C++ program, hello world
+Licensed under GNU General Public License 3.0
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello world! :^)\n"; //Prints hello world, of course
+}
