42106dcfab45fd3d3803ec01c266e0b86269c20c
Eoin <eoinmccoy@me.com>
2016-04-21 18:21:48 +0100

    Update pi-to-n.cpp
    Program was previously printing to a set amount of digits including the characters "3." resulting in the program printing 2 less digits of Pi than the user would have intended. Fixed with printf.
    
    Validation added, if the user enters a number less than or equal to 0, greater than 100 or anything other than an integer, the program will print an error and re-request the input.

--- pi-to-n.cpp
+++ pi-to-n.cpp
@@ -2,22 +2,45 @@
 Generate Pi to the nth digit
 Licensed under GNU General Public License 3.0
 
+cinCheck() function taken from simple-calculator.cpp, see at github.com/yoghurt1/cpp
+
 Changelog
-  V1.0 - Created, 20/4/2016
+  V1.0 - Original program
+  V2.0 - Fixed issue where setprecision() was ignoring the first two characters, added validation
 */
 
 #include <iostream>
-#include <iomanip> //For setprecision
+#include <stdio.h> //For printf
+#include <limits> //For numeric_limits
+
 using namespace std;
+
+int cinCheck() //Function for checking cin for nth
+{
+  if ( !cin ) { //If cin is invalid
+    cin.clear(); //Clear failbit
+    cin.ignore(); //Clear stream
+  }
+
+  return 0; //End cinCheck()
+}
 
 int main()
 {
 
   int nth; //Initialize nth variable to store nth digit
-  double pi = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679; //Initialise pi variable to 100 digits of Pi
-  cout << "Welcome to the Generate Pi to the nth digit program!\nPlease enter a number to generate Pi to (Up to 100): "; //Print greeting
+  cout << "Please enter a number to generate Pi to (Up to 100): "; //Print greeting
   cin >> nth; //Get how many digits of Pi the user wants
-  cout << setprecision(nth) << pi << "\n"; //Print Pi to user-defined amount of digits
+  cinCheck(); //Run cinCheck function
+
+
+  while ( nth > 100 || nth <= 0) { //While nth is greater than 100 or less than or equal to 0
+    cout << "That input was invalid. Please enter a number between 1 and 100.\n"; //Print error
+    cin >> nth; //Get nth value again
+  }
+
+  cout << "Here is Pi to " << nth << " digit(s): ";
+  printf ("%.*s\n", nth + 2, "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679"); //Print Pi to user-defined amount of digits
 
   return 0;
 
