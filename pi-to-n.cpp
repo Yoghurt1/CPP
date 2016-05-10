6a127575b1867fe799442720e0854b1d230f8aad
Eoin <eoinmccoy@me.com>
2016-05-10 14:26:14 +0100

    Update pi-to-n.cpp
    Added a few comments to the if statements at the end of the program.

--- pi-to-n.cpp
+++ pi-to-n.cpp
@@ -9,6 +9,7 @@ Changelog
   V1.0 - Original program
   V2.0 - Fixed issue where setprecision() was ignoring the first two characters, added validation
   V3.0 - Added e generation
+  V3.1 - Added comments
 */
 
 #include <iostream>
@@ -51,12 +52,12 @@ int main()
     cinCheck(); //Run cinCheck()
   }
 
-  if ( piOre == "Pi" || piOre == "pi" ) {
-    cout << "Here is Pi to " << nth << " digit(s): ";
+  if ( piOre == "Pi" || piOre == "pi" ) { //If user chose pi
+    cout << "Here is Pi to " << nth << " digit(s): "; //Output intro
     printf ("%.*s\n", nth + 2, "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679"); //Print Pi to user-defined amount of digits
   }
-  else if ( piOre == "e" || piOre == "E" ) {
-    cout << "Here is e to " << nth << " digit(s): ";
+  else if ( piOre == "e" || piOre == "E" ) { //If user chose e
+    cout << "Here is e to " << nth << " digit(s): "; //Output intro
     printf ("%.*s\n", nth + 2, "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274"); //Print e to user-defined amount of digits
   }
 
